8e67c26d6177f3696124ae6f1808e0d78f89bd92
Wahidullah Rahimi <wahidrahimi45@gmail>
2025-12-26 21:33:52 -0800

    Set up the order module

--- e-commerce-app/src/test/java/com/cwc/e_commerce_app/ECommerceAppApplicationTests.java
+++ e-commerce-app/src/test/java/com/cwc/e_commerce_app/ECommerceAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.cwc.e_commerce_app;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class ECommerceAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void sampleTest() {
+	    // Sample test case
+		Assertions.assertEquals(2, 1 + 1);
 	}
 
 }
